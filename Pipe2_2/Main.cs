40599bd0285f1864849c731c9addf4cc320cde18
Ячменев Дмитрий <dmsdrdp@gmail.com>
2023-01-07 00:57:37 +0300

    Добавьте файлы проекта.

---
+++ Pipe2_2/Main.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Задание 2.2 Количество труб на активном виде
+
+namespace Pipe2_2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var fInstances = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                           .OfClass(typeof(Pipe))
+                           .Cast<Pipe>()
+                           .ToList();
+
+            TaskDialog.Show("Pipes count", fInstances.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
